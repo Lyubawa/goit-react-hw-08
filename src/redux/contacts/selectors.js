bd795a3b5667eb36d7973b78ed183506a5ea06d0
Lyubawa <lyubawa81@ukr.net>
2024-04-01 20:38:10 +0300

    hw-react-8-4

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,25 +1,2 @@
-// import { createSelector } from '@reduxjs/toolkit';
-// import { selectContatcsFilter } from '../filters/slice';
-
 export const selectLoading = state => state.contacts.loading;
 export const selectContacts = state => state.contacts.items;
-// export const selectFilterContacts = state => state.contacts.filter;
-// export const selectError = state => state.contacts.error;
-// MEMO
-// export const selectFilterContacts = createSelector(
-//   [selectContacts, selectContatcsFilter],
-//   (contacts, contactsFilter) => {
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(contactsFilter.toLowerCase())
-//     );
-//   }
-// );
-
-// З ЦЬОГО РОБИТИ МЕМО
-// export const selectFilterContacts = state => {
-//   const contacts = selectContacts(state);
-//   const contactsFilter = selectContatcsFilter(state);
-//   return contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(contactsFilter.toLowerCase())
-//   );
-// };
